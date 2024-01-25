83b73702b65246754fca3739f61c6f5d73695f3d
savelmtr <savelmtr@gmail.com>
2024-01-25 13:38:45 +0300

    update states

--- lib/states.py
+++ lib/states.py
@@ -2,14 +2,6 @@ from telebot.asyncio_handler_backends import State, StatesGroup
 
 
 class States(StatesGroup):
-    welcome_workflow = State()
     update_birthday = State()
-    join_group = State()
-    create_group = State()
-    wish_list = State()
     update_name = State()
     update_wishes = State()
-    create_password = State()
-    enter_password = State()
-    max_price = State()
-    rename = State()
