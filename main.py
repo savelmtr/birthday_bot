8d36813817efe389b851c4f67e89dfc4f16a51f9
savelmtr <savelmtr@gmail.com>
2024-02-11 09:19:22 +0300

    test

--- main.py
+++ main.py
@@ -40,11 +40,12 @@ bot.message_handler(content_types=['left_chat_member'])(user_go)
 
 TASKS = (
     {'cronstr': '0 3 16 * *', 'coro': monthly, 'iterator': None, 'next_ft': None},
-    {'cronstr': '30 6 * * *', 'coro': congrats, 'iterator': None, 'next_ft': None},
+    {'cronstr': '0 3 * * *', 'coro': congrats, 'iterator': None, 'next_ft': None},
 )
 
 
 async def main():
+    await congrats(bot)
     task = asyncio.create_task(tasks_manager(bot, TASKS))
     await bot.polling()
 
